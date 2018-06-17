fef0e21fcfb58546a2cfef596fc82411da14ac29
Pedro J. Ortiz <pedro@pjortiz.com>
2018-06-18 00:59:34 +0200

    Added support for 60 new encodings

--- README.md
+++ README.md
@@ -2,12 +2,82 @@
 
 Gutf is a terminal tool that converts files encoding to UTF-8.
 
-For the moment it only converts ISO8859_1 to UTF-8 but more encodings will be added later. This tool will only convert files into UTF-8 because, let's be honest, using something different than UTF-8 today is just unnaceptable.
+This tool will only convert files into UTF-8 because, let's be honest, using something different than UTF-8 today is just unacceptable.
 
 Different from iconv, gutf works with huge files as it does not load the whole file into memory.
 
 ## Usage
 
 ```text
-gutf inputFile outputFile
+gutf [input] [encoding] [output]
 ```
+
+## Supported Encodings
+
+```text
+IBMCodePage037
+IBMCodePage437
+IBMCodePage850
+IBMCodePage852
+IBMCodePage855
+WindowsCodePage858
+IBMCodePage860
+IBMCodePage862
+IBMCodePage863
+IBMCodePage865
+IBMCodePage866
+IBMCodePage1047
+IBMCodePage1140
+ISO8859-1
+ISO8859-2
+ISO8859-3
+ISO8859-4
+ISO8859-5
+ISO8859-6
+ISO-8859-6E
+ISO-8859-6I
+ISO8859-7
+ISO8859-8
+ISO-8859-8E
+ISO-8859-8I
+ISO8859-9
+ISO8859-10
+ISO8859-13
+ISO8859-14
+ISO8859-15
+ISO8859-16
+KOI8-R
+KOI8-U
+Macintosh
+MacintoshCyrillic
+Windows874
+Windows1250
+Windows1251
+Windows1252
+Windows1253
+Windows1254
+Windows1255
+Windows1256
+Windows1257
+Windows1258
+X-User-Defined
+EUC-JP
+ISO-2022-JP
+ShiftJIS
+EUC-KR
+GB18030
+GBK
+HZ-GB2312
+Big5
+UTF-8
+UTF-16BE(UseBOM)
+UTF-16BE(IgnoreBOM)
+UTF-16LE(IgnoreBOM)
+UTF-32BE(UseBOM)
+UTF-32BE(IgnoreBOM)
+UTF-32LE(IgnoreBOM)
+```
+
+## Todo
+
+Ok ok, maybe there is an unholy reason why you would want to convert the encoding of a file into something different than UTF-8, I'll add support later so that you can do this forbidden conversions.
--- gutf.go
+++ gutf.go
@@ -6,23 +6,92 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
+	"unicode"
 
+	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/encoding/korean"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
+	eunicode "golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/encoding/unicode/utf32"
 )
 
+// StripSpaces removes the spaces from a string
+func StripSpaces(s string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, s)
+}
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "usage:\n\t%s [input] [output]\n", os.Args[0])
+
+	if len(os.Args) != 4 {
+		fmt.Fprintf(os.Stderr, "usage:\n\t%s [input] [encoding] [output]\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	f, err := os.Open(os.Args[1])
+	count := 0
+
+	m := make(map[string]encoding.Encoding)
+	for _, v := range charmap.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	for _, v := range japanese.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	for _, v := range korean.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	for _, v := range simplifiedchinese.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	for _, v := range traditionalchinese.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	for _, v := range eunicode.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	for _, v := range utf32.All {
+		s := StripSpaces(fmt.Sprint(v))
+		count++
+		m[s] = v
+	}
+
+	fmt.Println(count)
+
+	input, iencoding, output := os.Args[1], os.Args[2], os.Args[3]
+
+	f, err := os.Open(input)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	outFile, err := os.Create(os.Args[2])
+	outFile, err := os.Create(output)
 
 	if err != nil {
 		log.Fatal(err)
@@ -32,11 +101,11 @@ func main() {
 
 	out := bufio.NewWriterSize(outFile, 1<<20)
 
-	r := charmap.ISO8859_1.NewDecoder().Reader(in)
+	decode := m[iencoding].NewDecoder().Reader(in)
 
 	buf := make([]byte, 1<<20)
 
-	io.CopyBuffer(out, r, buf)
+	io.CopyBuffer(out, decode, buf)
 
 	out.Flush()
 	outFile.Close()
